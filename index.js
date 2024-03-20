25e299f6f413fc72adb43293cb8b8d0edfcf19a7
kenyantequila <abdishakur431@gmail.com>
2024-03-20 17:09:50 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+var currentUser ="Grace Hopper";
+var welcomeMessage ="Welcome to Flatbook,";
+var welcomeMessage ="Grace Hopper";
+var welcomeMessage ="Welcome to Flatbook, Grace Hopper!"
+var excitedWelcomeMessage = welcomeMessage.toUpperCase();
+var shortGreeting = "Welcome, ${Grace.slice(0, 1)}!";
+
